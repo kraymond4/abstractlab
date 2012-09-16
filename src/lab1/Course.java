6d1a8908e1b18a44fe6ccce27231e508586cdc42
Owner <Owner@Owner-PC>
2012-09-16 13:02:12 -0500

    Lab one of abstract lab v 1.0 added abstract class "Course"

--- src/lab1/Course.java
+++ src/lab1/Course.java
@@ -15,5 +15,15 @@ public abstract class Course
     private String courseNumber;
     private double credits;
     
+    public abstract String getCourseNumber();
     
+    public abstract void setCourseNumber(String courseNumber);
+    
+    public abstract double getCredits();
+    
+    public abstract void setCredits(double credits);
+    
+    public abstract String getCourseName();
+    
+    public abstract void setCourseName(String courseName);
 }
