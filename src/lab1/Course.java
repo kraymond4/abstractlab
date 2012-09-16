8aa3e6b3e752d9480f10a75e755a934aa9f8b9a0
Owner <Owner@Owner-PC>
2012-09-16 12:54:21 -0500

    Lab one of abstract lab v 1.0 added abstract class "Course"

---
+++ src/lab1/Course.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Kyle Raymond
+ * version: 1.0
+ */
+public abstract class Course 
+{
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    
+    
+}
