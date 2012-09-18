c8debe2572ea030b068dbddbaf8163c1c7093652
kraymond4 <kraymond4@L11111038630.wctc.msft>
2012-09-18 14:06:07 -0500

    working on it

--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -10,11 +10,12 @@ public class IntroJavaCourse extends Course {
     
     private String prerequisites;
 
-    public IntroJavaCourse(String courseName, String courseNumber, double credits) 
+    public IntroJavaCourse(String courseName, String courseNumber, double credits, String prerequisites) 
     {
         this.setCourseName(courseName);
         this.setCourseNumber(courseNumber);
         this.setCredits(credits);
+        this.setPrerequisites(prerequisites);
     }
 
    
