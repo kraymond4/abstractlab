8c7232fe24c22e13d56db0c4ebd3e5b54ff07b24
kraymond4 <kraymond4@L11111038630.wctc.msft>
2012-09-18 14:07:55 -0500

    working on it

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -1,6 +1,6 @@
 package lab1;
 
-import javax.swing.JOptionPane;
+
 
 /**
  * Describe responsibilities here. NOTE: this class has NO PREREQUISITES!
@@ -9,54 +9,29 @@ import javax.swing.JOptionPane;
  * @author      Kyle Raymond
  * @version     1.05
  */
-public class IntroToProgrammingCourse {
-    private String courseName;
-    private String courseNumber;
-    private double credits;
+public class IntroToProgrammingCourse extends Course 
+{
+    
 
-    public IntroToProgrammingCourse(String courseName, String courseNumber) {
+    public IntroToProgrammingCourse(String courseName, String courseNumber, double credits) 
+    {
         this.setCourseName(courseName);
         this.setCourseNumber(courseNumber);
+        this.setCredits(credits);
     }
 
-    public String getCourseNumber() {
-        return courseNumber;
-    }
+   
 
-    public final void setCourseNumber(String courseNumber) {
-        if(courseNumber == null || courseNumber.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: courseNumber cannot be null of empty string");
-            System.exit(0);
-        }
-        this.courseNumber = courseNumber;
-    }
+    
+        
 
-    public double getCredits() {
-        return credits;
-    }
+    
 
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.credits = credits;
-    }
+    
 
-    public String getCourseName() {
-        return courseName;
-    }
+    
 
-    public final void setCourseName(String courseName) {
-        if(courseName == null || courseName.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: courseName cannot be null of empty string");
-            System.exit(0);
-        }
-        this.courseName = courseName;
-    }
+    
 
     
 }
