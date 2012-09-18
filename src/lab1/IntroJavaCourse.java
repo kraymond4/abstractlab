75ffb403fbce00573a8c4be6bf80fc6f3307e786
kraymond4 <kraymond4@L11111038630.wctc.msft>
2012-09-18 14:05:02 -0500

    working on it

--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -6,45 +6,35 @@ package lab1;
  * @author      Kyle Raymond
  * @version     1.05
  */
-public class IntroJavaCourse {
-    private String courseName;
-    private String courseNumber;
-    private double credits;
+public class IntroJavaCourse extends Course {
+    
     private String prerequisites;
 
-    public IntroJavaCourse(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
+    public IntroJavaCourse(String courseName, String courseNumber, double credits) 
+    {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+        this.setCredits(credits);
     }
 
-    public String getCourseNumber() {
-        return courseNumber;
-    }
+   
 
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
+    
 
-    public double getCredits() {
-        return credits;
-    }
+    
 
 
-    public String getPrerequisites() {
+    public final String getPrerequisites() 
+    {
         return prerequisites;
     }
 
-    public void setPrerequisites(String prerequisites) {
+    public final void setPrerequisites(String prerequisites) 
+    {
         this.prerequisites = prerequisites;
     }
 
-        public void setCredits(double credits) {
-        if(credits < 0 || credits > 5.0) {
-            System.out.println(
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.setCredits(credits);
-    }
+        
+        
 
 }
