d91de234b97364721d310075eecb545b27548d4c
Owner <Owner@Owner-PC>
2012-09-21 14:35:08 -0500

    Lab two of abstract lab v 1.05 added abstract interface "CourseOutline"

--- src/lab2/IntroJavaCourse.java
+++ src/lab2/IntroJavaCourse.java
@@ -6,39 +6,60 @@ package lab2;
  * @author      your name goes here
  * @version     1.00
  */
-public class IntroJavaCourse {
-    String courseName;
+public class IntroJavaCourse implements CourseOutline
+{
+    private String courseName;
     private String courseNumber;
     private double credits;
     private String prerequisites;
 
-    public IntroJavaCourse(String courseName, String courseNumber) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
+    public IntroJavaCourse(String courseName, String courseNumber, double credits, String prerequisites) 
+    {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+        this.setCredits(credits);
+        this.setPrerequisites(prerequisites);
+        
     }
 
-    public String getCourseNumber() {
+    public String getCourseNumber() 
+    {
         return courseNumber;
     }
 
-    public void setCourseNumber(String courseNumber) {
+    public void setCourseNumber(String courseNumber) 
+    {
         this.courseNumber = courseNumber;
     }
 
-    public double getCredits() {
+    public double getCredits() 
+    {
         return credits;
     }
 
-    public void setCredits(double credits) {
+    public void setCredits(double credits) 
+    {
         this.credits = credits;
     }
 
-    public String getPrerequisites() {
+    public String getPrerequisites() 
+    {
         return prerequisites;
     }
 
-    public void setPrerequisites(String prerequisites) {
+    public void setPrerequisites(String prerequisites) 
+    {
         this.prerequisites = prerequisites;
+    }
+
+    public String getCourseName() 
+    {
+        return this.courseName;
+    }
+
+    public void setCourseName(String courseName) 
+    {
+        this.courseName = courseName;
     }
 
     
