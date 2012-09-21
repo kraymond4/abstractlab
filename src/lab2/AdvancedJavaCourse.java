895c68d18c95118baa5dd716572a1312e650fc80
Owner <Owner@Owner-PC>
2012-09-21 14:38:18 -0500

    Lab two of abstract lab v 1.05 added abstract interface "CourseOutline"

--- src/lab2/AdvancedJavaCourse.java
+++ src/lab2/AdvancedJavaCourse.java
@@ -1,6 +1,6 @@
 package lab2;
 
-import javax.swing.JOptionPane;
+
 
 /**
  * Describe responsibilities here.
@@ -8,66 +8,62 @@ import javax.swing.JOptionPane;
  * @author      your name goes here
  * @version     1.00
  */
-public class AdvancedJavaCourse {
+public class AdvancedJavaCourse implements CourseOutline
+{
     private String courseName;
     private String courseNumber;
     private double credits;
     private String prerequisites;
 
-    public AdvancedJavaCourse(String courseName, String courseNumber) {
+    public AdvancedJavaCourse(String courseName, String courseNumber, double credits, String prerequisites) 
+    {
         this.setCourseName(courseName);
         this.setCourseNumber(courseNumber);
+        this.setCredits(credits);
+        this.setPrerequisites(prerequisites);
     }
 
-    public String getCourseNumber() {
+    public String getCourseNumber() 
+    {
         return courseNumber;
     }
 
-    public final void setCourseNumber(String courseNumber) {
-        if(courseNumber == null || courseNumber.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: courseNumber cannot be null of empty string");
-            System.exit(0);
-        }
+    public final void setCourseNumber(String courseNumber) 
+    {
+        //needs validation
         this.courseNumber = courseNumber;
     }
 
-    public double getCredits() {
+    public double getCredits() 
+    {
         return credits;
     }
 
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
+    public void setCredits(double credits) 
+    {
+        //needs validation
         this.credits = credits;
     }
 
-    public String getPrerequisites() {
+    public String getPrerequisites() 
+    {
         return prerequisites;
     }
 
-    public void setPrerequisites(String prerequisites) {
-        if(prerequisites == null || prerequisites.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: prerequisites cannot be null of empty string");
-            System.exit(0);
-        }
+    public void setPrerequisites(String prerequisites) 
+    {
+        //needs validation
         this.prerequisites = prerequisites;
     }
 
-    public String getCourseName() {
+    public String getCourseName() 
+    {
         return courseName;
     }
 
-    public final void setCourseName(String courseName) {
-        if(courseName == null || courseName.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: courseName cannot be null of empty string");
-            System.exit(0);
-        }
+    public final void setCourseName(String courseName) 
+    {
+        //needs validation
         this.courseName = courseName;
     }
 
